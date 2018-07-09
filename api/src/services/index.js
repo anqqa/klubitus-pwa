75d3067f8a642aaf387c807d6ade6686d4147c66
anqqa <anttiq@gmail.com>
2018-07-09 12:32:03 +0300

    Remove old Feather based API

--- api/src/services/index.js
+++
@@ -1,9 +0,0 @@
-const events = require('./events/events.service.js');
-const users = require('./users/users.service.js');
-
-module.exports = function () {
-  const app = this; // eslint-disable-line no-unused-vars
-
-  app.configure(users);
-  app.configure(events);
-};
