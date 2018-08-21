1fe56f689d3a4eb83f2e75d6c12a36890334e7e8
anqqa <anttiq@gmail.com>
2018-08-21 17:32:52 +0300

    Support null urls

--- client/utils/url.js
+++ client/utils/url.js
@@ -1,1 +1,1 @@
-export const avatarUrl = url => url.startsWith('avatar/') ? `//localhost:3000/${url}` : url;
+export const avatarUrl = url => url && url.startsWith('avatar/') ? `//localhost:3000/${url}` : url;
