5002921f8752427de78f0a69f8813dafc2d346b8
anqqa <anttiq@gmail.com>
2018-09-01 15:16:31 +0300

    Breadcrumbs component

---
+++ client/components/Breadcrumbs.vue
@@ -0,0 +1,24 @@
+<template>
+  <nav aria-label="Breadcrumb">
+    <ol>
+      <li v-for="(trail, index) in breadcrumbs" :key="index">
+        <nuxt-link v-if="trail.current" :to="trail.url" aria-current="page">{{ trail.title }}</nuxt-link>
+        <nuxt-link v-else :to="trail.url">{{ trail.title }}</nuxt-link>
+      </li>
+    </ol>
+  </nav>
+</template>
+
+
+<script>
+  export default {
+    props: {
+      breadcrumbs: { default: () => [], type: Array },
+    },
+  };
+</script>
+
+
+<style scoped>
+
+</style>
