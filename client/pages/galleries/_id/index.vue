f84522a7aa97578f32c41e7566156e9b394781cb
anqqa <anttiq@gmail.com>
2018-09-01 15:16:54 +0300

    Add breadcrumbs

--- client/pages/galleries/_id/index.vue
+++ client/pages/galleries/_id/index.vue
@@ -1,8 +1,9 @@
 <template>
 
   <main>
+    <Breadcrumbs :breadcrumbs="breadcrumbs" />
     <h1>
-      {{ gallery.name }}
+      {{ eventName }}
       <small> - {{ format(gallery.image_count) }} images</small>
     </h1>
 
@@ -13,6 +14,9 @@
 
 
 <script>
+  import format from 'date-fns/format';
+
+  import Breadcrumbs from '../../../components/Breadcrumbs';
   import ImageList from '../../../components/galleries/ImageList';
 
 
@@ -29,10 +33,37 @@
       return { gallery, images };
     },
 
-    components: { ImageList },
+    components: { Breadcrumbs, ImageList },
 
     data() {
       return { url: `${this.$route.path}/:imageId` };
+    },
+
+    computed: {
+      back() { return this.breadcrumbs[this.breadcrumbs.length - 2]; },
+
+      breadcrumbs() {
+        return [
+          { url: this.localePath({ name: 'galleries' }), title: 'Galleries' },
+          { url: this.localePath({ name: 'galleries-events-year-month-day' }), title: 'Events' },
+          { url: this.localePath({
+              name: 'galleries-events-year-month-day',
+              params: { year: this.eventDate.getFullYear() },
+            }),
+            title: format(this.eventDate, 'YYYY'),
+          },
+          { url: this.localePath({
+              name: 'galleries-events-year-month-day',
+              params: { year: this.eventDate.getFullYear(), month: this.eventDate.getMonth() + 1 },
+            }),
+            title: format(this.eventDate, 'MMMM'),
+          },
+          { url: this.$route.fullPath, title: this.eventName, current: true },
+        ];
+      },
+
+      eventDate() { return new Date(this.gallery.event ? this.gallery.event.begins_at : this.gallery.event_date); },
+      eventName() { return this.gallery.event ? this.gallery.event.name : this.gallery.name; },
     },
 
     methods: {
