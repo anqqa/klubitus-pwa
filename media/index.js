dfb66e98466db91a2ef8cf4dd2129b99330b60df
anqqa <anttiq@gmail.com>
2019-01-07 12:10:55 +0200

    Add support for format conversion (additional extension) and automatic height/width (dimension to 0)

--- media/index.js
+++ media/index.js
@@ -10,39 +10,73 @@ const URL                 = process.env.URL;
 const ALLOWED_RESOLUTIONS = process.env.ALLOWED_RESOLUTIONS
   ? new Set(process.env.ALLOWED_RESOLUTIONS.split(/\s*,\s*/))
   : new Set([]);
+const ALLOWED_TARGETS     = new Set((process.env.ALLOWED_TARGETS || 'jpeg,png,webp').split(/\s*,\s*/));
 
 exports.handler = function(event, context, callback) {
   const key   = event.queryStringParameters.key;
-  const match = key.match(/((\d+)x(\d+))\/(.*)/);
 
   console.log(`Received ${key}`);
 
-  // Check if requested resolution is allowed
-  if (0 != ALLOWED_RESOLUTIONS.size && !ALLOWED_RESOLUTIONS.has(match[1])) {
-    callback(null, {
-      statusCode: '403',
-      headers:    {},
-      body:       '',
-    });
+  //  const match = key.match(/((\d+)x(\d+))\/(.*)/);
+  // Matches r/123x456/some/path/file.png.webp
+  // const match = key.match(/(?<method>\w)\/(?<res>(?<width>\d+)x(?<height>\d+))\/(?<orig>([^.]+)\.(\w+))(\.(?<target>\w+))?/);
+  const match = key.match(/(\w)\/((\d+)x(\d+))\/(([^.]+)\.(\w+))(\.(\w+))?/);
+
+  if (!match) {
+    console.log(' - no match');
+
+    callback(null, { body: '', headers: {}, statusCode: '404' });
 
     return;
   }
 
-  const width       = parseInt(match[2], 10);
-  const height      = parseInt(match[3], 10);
-  const originalKey = match[4];
+  const [full, method, res, width, height, orig, name, filetype, targetExt, target] = match;
 
-  console.log(` - resizing ${originalKey} to ${width}x${height}`);
+  if (method !== 'r') {
+    console.log(' - invalid method', method);
+
+    callback(null, { body: '', headers: {}, statusCode: '403' });
+
+    return;
+  }
+
+  // Check if requested resolution is allowed
+  if (ALLOWED_RESOLUTIONS.size !== 0 && !ALLOWED_RESOLUTIONS.has(res)) {
+    console.log(' - resolution not allowed', res, ALLOWED_RESOLUTIONS);
+
+    callback(null, { body: '', headers: {}, statusCode: '403' });
+
+    return;
+  }
+
+  // Check if requested format is allowed
+  if (target && !ALLOWED_TARGETS.has(target)) {
+    console.log(' - target not allowed', target, ALLOWED_TARGETS);
+
+    callback(null, { body: '', headers: {}, statusCode: '403' });
+
+    return;
+  }
+
+  const targetWidth  = parseInt(width, 10) || null;
+  const targetHeight = parseInt(height, 10) || null;
+  const originalKey  = orig;
+
+  console.log(` - resizing ${originalKey} to ${targetWidth || 'auto'}x${targetHeight || 'auto'} ${target || ''}`);
 
   S3.getObject({ Bucket: BUCKET, Key: originalKey }).promise()
     .then(data => Sharp(data.Body)
-      .resize(width, height)
+      .resize(targetWidth, targetHeight)
+      .crop(Sharp.strategy.attention)
+      .jpeg({ force: target === 'jpeg' })
+      .png({ force: target === 'png' })
+      .webp({ force: target === 'webp' })
       .toBuffer()
     )
     .then(buffer => S3.putObject({
         Body:        buffer,
         Bucket:      BUCKET,
-        ContentType: mime.lookup(originalKey),
+        ContentType: mime.lookup(target || originalKey),
         Key:         key,
       }).promise()
     )
