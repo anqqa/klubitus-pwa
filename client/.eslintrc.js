fc2fa42c695a59102757fbd8495f6d18d4be4eaf
anqqa <anttiq@gmail.com>
2019-02-25 17:12:12 +0200

    Add prettier

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,19 +1,23 @@
 module.exports = {
-  root:          true,
-  parserOptions: { parser: 'babel-eslint' },
-  env:           {
+  root: true,
+  parserOptions: { parser: "babel-eslint" },
+  env: {
     browser: true,
-    node:    true
+    node: true
   },
-  extends:       ['eslint:recommended', 'plugin:vue/recommended'],
+  extends: [
+    "eslint:recommended",
+    "plugin:vue/recommended",
+    "plugin:prettier/recommended"
+  ],
   // required to lint *.vue files
-  plugins:       ['vue'],
+  plugins: ["vue"],
   // add your custom rules here
-  rules:         {
-    'vue/html-closing-bracket-newline': 'off',
-    'vue/max-attributes-per-line': 'off',
-    'vue/multiline-html-element-content-newline': 'off',
-    'vue/singleline-html-element-content-newline': 'off',
+  rules: {
+    "vue/html-closing-bracket-newline": "off",
+    "vue/max-attributes-per-line": "off",
+    "vue/multiline-html-element-content-newline": "off",
+    "vue/singleline-html-element-content-newline": "off"
   },
-  globals:       {}
+  globals: {}
 };
