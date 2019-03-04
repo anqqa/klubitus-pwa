df00a5648c54106694302079ee2ae5b6c0515a1b
anqqa <anttiq@gmail.com>
2019-03-04 14:25:14 +0200

    Style fixes

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -8,8 +8,12 @@ module.exports = {
   // add your custom rules here
   plugins: ['vue'],
   rules: {
-    'vue/html-closing-bracket-newline': 'off',
-    'vue/max-attributes-per-line': 'off',
+    'vue/component-name-in-template-casing': ['error', 'kebab-case'],
+    'vue/html-closing-bracket-newline': ['error', { multiline: 'never' }],
+    'vue/max-attributes-per-line': [
+      'error',
+      { singleline: 5, multiline: { allowFirstLine: true } },
+    ],
     'vue/multiline-html-element-content-newline': 'off',
     'vue/singleline-html-element-content-newline': 'off',
   },
