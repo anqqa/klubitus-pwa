17957cd51de7007a1a66c8122da4b79c35799ee9
anqqa <anttiq@gmail.com>
2019-03-11 17:58:31 +0200

    Continue as name

--- client/src/components/FBLogin.vue
+++ client/src/components/FBLogin.vue
@@ -1,7 +1,8 @@
 <template>
   <button v-if="isSdkReady" class="button is-full is-outlined" @click="onClick">
     <span class="icon"><i class="bx bxl-facebook"/></span>
-    Login with Facebook
+    <span v-if="name">Continue as {{ name }}</span>
+    <span v-else>Login with Facebook</span>
   </button>
 </template>
 
@@ -16,49 +17,102 @@ export default {
   data() {
     return {
       isSdkReady: false,
+      name: null,
+      token: null,
     };
   },
 
-  mounted() {
-    this.initFbSdk();
+  computed: {
+    isConnected() {
+      return !!this.token;
+    },
+  },
+
+  watch: {
+    async isConnected(connected) {
+      if (connected) {
+        this.$emit('connected');
+
+        const response = await this.getMe('name');
+
+        this.name = response.name;
+      }
+    },
+  },
+
+  async mounted() {
+    try {
+      await this.initFbSdk();
+      this.isSdkReady = true;
+
+      const response = await this.getStatus();
+      console.log('getStatus', response);
+      if (response.status === 'connected') {
+        this.token = response.authResponse.accessToken;
+      }
+    } catch (error) {
+      console.warn(error);
+    }
   },
 
   methods: {
-    initFbSdk() {
-      const installScript = (doc, id) => {
-        if (doc.getElementById(id)) {
-          this.isSdkReady = true;
-
-          return;
-        }
-
-        const first = doc.getElementsByTagName('script')[0];
-        const sdkJs = doc.createElement('script');
-        sdkJs.id = id;
-        sdkJs.src = 'https://connect.facebook.net/en_US/sdk.js';
-        first.parentNode.insertBefore(sdkJs, first);
-      };
-
-      installScript(document, 'fb-sdk');
-
-      window.fbAsyncInit = () => {
-        FB.init({
-          appId: process.env.FB_APP_ID,
-          cookie: true,
-          xfbml: false,
-          version: 'v3.2',
-        });
-
-        this.isSdkReady = true;
-      };
+    async getMe(fields = 'id') {
+      return new Promise(resolve => {
+        window.FB.api(`/me?fields=${fields}`, response => resolve(response));
+      });
     },
 
-    onClick(event) {
+    async getStatus() {
+      return new Promise(resolve => {
+        window.FB.getLoginStatus(response => resolve(response));
+      });
+    },
+
+    async initFbSdk() {
+      return new Promise(resolve => {
+        window.fbAsyncInit = () => {
+          window.FB.init({
+            appId: process.env.FB_APP_ID,
+            cookie: true,
+            xfbml: false,
+            version: 'v3.2',
+          });
+
+          resolve();
+        };
+
+        ((d, s, id) => {
+          if (d.getElementById(id)) {
+            resolve();
+
+            return;
+          }
+
+          const fjs = d.getElementsByTagName(s)[0];
+          const js = d.createElement('script');
+          js.id = id;
+          js.src = 'https://connect.facebook.net/en_US/sdk.js';
+          fjs.parentNode.insertBefore(js, fjs);
+        })(document, 'script', 'facebook-jssdk');
+      });
+    },
+
+    async login(options) {
+      return new Promise(resolve => {
+        window.FB.login(response => resolve(response), options);
+      });
+    },
+
+    async onClick(event) {
       event.preventDefault();
 
-      window.FB.login(response => {
-        this.$emit(response.authResponse ? 'success' : 'error', response);
-      }, this.params);
+      const response = await this.login(this.params);
+      if (response.status === 'connected') {
+        this.token = response.authResponse.accessToken;
+      }
+
+      console.log('onClick', response);
+      this.$emit(response.authResponse ? 'success' : 'error', response);
     },
   },
 };
