9c94dfe698245ca03d8b1518df05d62e6f1c6b29
anqqa <anttiq@gmail.com>
2019-05-03 15:25:28 +0300

    Fix .params() not working in asyncData when server side rendering

--- client/src/models/Model.js
+++ client/src/models/Model.js
@@ -1,27 +1,22 @@
 import { Model as BaseModel } from 'vue-api-query';
 
 export default class Model extends BaseModel {
-  find(identifier) {
-    if (identifier === undefined) {
-      throw new Error('You must specify the param on find() method.');
-    }
-
-    let base = this._fromResource || `${this.baseURL()}/${this.resource()}`;
-    let url = `${base}/${identifier}${this._builder.query()}`;
-
-    return this.request({
-      url,
-      method: 'GET',
-    }).then(response => new this.constructor(response.data.data || response.data));
-  }
-
   baseURL() {
-    console.log(this.$http);
     return this.$http.defaults.baseURL.replace(/\/$/, '');
   }
 
+  /**
+   * @TODO: Remove after .params() works with SSR in asyncData.
+   */
+  static params(payload) {
+    const self = this.instance();
+
+    self._builder.payload = payload;
+
+    return self;
+  }
+
   request(config) {
-    console.log(config);
     return this.$http.request(config);
   }
 }
