97a18ea497e4b86b1e4fd1d91af101403de05fda
anqqa <anttiq@gmail.com>
2019-05-04 14:59:39 +0300

    Add missing forum module

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -3,10 +3,9 @@ import { Connection } from 'typeorm';
 
 import DatabaseModule from './db.module';
 import { EventsModule } from './events/events.module';
+import { ForumModule } from './forum/forum.module';
 
-@Module({
-  imports: [DatabaseModule, EventsModule],
-})
+@Module({ imports: [DatabaseModule, EventsModule, ForumModule] })
 export class ApplicationModule {
   constructor(private readonly connection: Connection) {}
 }
