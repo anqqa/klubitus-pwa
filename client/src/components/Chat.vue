e1abff5a4fbee836dc2976e276bf38e78b93172c
anqqa <anttiq@gmail.com>
2019-05-13 15:21:23 +0300

    Shout model

--- client/src/components/Chat.vue
+++ client/src/components/Chat.vue
@@ -1,69 +1,63 @@
 <template>
   <section class="card">
-
     <header>
       <h2>Chat</h2>
     </header>
 
     <ul class="card-content">
       <li v-for="shout in shouts" :key="shout.id" class="media">
-
         <div class="media-left">
-          <Avatar :image-url="shout.avatar" :name="shout.author.username" class="is-24x24" />
+          <avatar :image-url="shout.avatar" :name="shout.author.username" class="is-24x24" />
         </div>
 
         <div class="media-content">
           <nuxt-link class="user" to="/">{{ shout.author.username }}</nuxt-link>
           <time :datetime="shout.created_at" :title="shout.created_at">{{ shout.stamp }}</time>
-          <br>
+          <br />
           <div class="markdown" v-html="shout.html" />
         </div>
       </li>
     </ul>
-
   </section>
 </template>
 
-
 <script>
-  import format from 'date-fns/format';
+import format from 'date-fns/format';
 
-  import Avatar from './Avatar';
-  import { avatarUrl } from '../utils/url';
+import Avatar from './Avatar';
+import Shout from '../models/Shout';
+import { avatarUrl } from '../utils/url';
 
-  export default {
-    name: 'Chat',
+export default {
+  name: 'Chat',
 
-    components: { Avatar },
+  components: { Avatar },
 
-    props: {
-      limit: { default: 10, type: Number },
+  props: {
+    limit: { default: 10, type: Number },
+  },
+
+  asyncComputed: {
+    async shouts() {
+      const data = await Shout.limit(Math.min(this.limit || 10, 50)).get();
+
+      return data
+        .map(shout => ({
+          ...shout,
+          avatar: avatarUrl(shout.author.avatar_url),
+          html: this.$md.render(shout.shout),
+          stamp: format(shout.created_at, 'HH:mm'),
+        }))
+        .reverse();
     },
-
-    asyncComputed: {
-      async shouts() {
-        const { data } = await this.$axios.$get(`shouts`, {
-          params: { limit: Math.min(this.limit || 10, 50) }
-        });
-
-        data.forEach(shout => {
-            shout.avatar = avatarUrl(shout.author.avatar_url);
-            shout.html   = this.$md.render(shout.shout);
-            shout.stamp  = format(shout.created_at, 'HH:mm');
-        });
-
-        return data && data.reverse();
-      }
-    },
-
-  };
+  },
+};
 </script>
 
-
 <style scoped>
-  time {
-    color: var(--color-tertiary);
-    font-size: 75%;
-    margin-left: 10px;
-  }
+time {
+  color: var(--color-tertiary);
+  font-size: 75%;
+  margin-left: 10px;
+}
 </style>
---
+++ client/src/models/Shout.js
@@ -0,0 +1,7 @@
+import Model from './Model';
+
+export default class Shout extends Model {
+  // resource() {
+  //   return 'images';
+  // }
+}
