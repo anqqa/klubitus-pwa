c33cb30ca6b0245a24f081fb6fe188be050252f6
anqqa <anttiq@gmail.com>
2019-05-27 19:51:16 +0300

    Log response status codes for requests

--- api/src/common/interceptors/requestlogger.interceptor.ts
+++ api/src/common/interceptors/requestlogger.interceptor.ts
@@ -1,23 +1,42 @@
-import { CallHandler, ExecutionContext, Injectable, Logger, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
-import { tap } from 'rxjs/operators';
+import {
+  CallHandler,
+  ExecutionContext,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
 
 @Injectable()
 export class RequestLoggerInterceptor implements NestInterceptor {
   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
     const startTime = Date.now();
-    const request = context.switchToHttp().getRequest();
+    const { raw: req } = context.switchToHttp().getRequest();
 
-    return next
-      .handle()
-      .pipe(
-        tap(() =>
-          Logger.debug(
-            `${request.raw.method} ${request.raw.url} ${Date.now() - startTime}ms`,
-            'Request',
-            false,
-          ),
-        ),
+    const logRequest = (status: number = HttpStatus.OK) =>
+      Logger.debug(
+        `${req.method} ${req.url} ${status} ${Date.now() - startTime}ms`,
+        'Request',
+        false,
       );
+
+    return next.handle().pipe(
+      tap(() => {
+        const { res } = context.switchToHttp().getResponse();
+
+        logRequest(res.statusCode);
+      }),
+      catchError((error: any) => {
+        const status =
+          error instanceof HttpException ? error.getStatus() : HttpStatus.INTERNAL_SERVER_ERROR;
+
+        logRequest(status);
+
+        return throwError(error);
+      }),
+    );
   }
 }
