51a2a9f3d2b7decfd40093241a373e486f9ef33a
anqqa <anttiq@gmail.com>
2019-06-13 15:16:54 +0300

    Fix dependencies after Nest upgrade

--- api/src/auth/auth.module.ts
+++ api/src/auth/auth.module.ts
@@ -1,6 +1,10 @@
 import { Module } from '@nestjs/common';
 import { PassportModule } from '@nestjs/passport';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
+import { External } from '../users/externals/external.entity';
+import { Token } from '../users/tokens/token.entity';
+import { User } from '../users/user.entity';
 import { UsersModule } from '../users/users.module';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
@@ -10,7 +14,11 @@ import { HttpStrategy } from './http.strategy';
 @Module({
   controllers: [AuthController],
   exports: [AuthService, PassportModule],
-  imports: [PassportModule.register({ defaultStrategy: 'bearer' }), UsersModule],
+  imports: [
+    PassportModule.register({ defaultStrategy: 'bearer' }),
+    TypeOrmModule.forFeature([External, Token, User]),
+    UsersModule,
+  ],
   providers: [AuthService, FacebookService, HttpStrategy],
 })
 export class AuthModule {}
