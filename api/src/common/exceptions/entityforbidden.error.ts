acbbc88f7ef332fcb221cc0e62295fac6b146f79
anqqa <anttiq@gmail.com>
2019-07-17 17:12:35 +0300

    Add EntityForbiddenError to be used when an authorized user doesn't have access to an entity

---
+++ api/src/common/exceptions/entityforbidden.error.ts
@@ -0,0 +1,18 @@
+export class EntityForbiddenError extends Error {
+  name = 'EntityForbidden';
+
+  constructor(entityClass: any) {
+    super();
+
+    Object.setPrototypeOf(this, EntityForbiddenError.prototype);
+
+    let targetName: string;
+    if (typeof entityClass === 'object') {
+      targetName = entityClass.constructor.name;
+    } else {
+      targetName = entityClass as string;
+    }
+
+    this.message = `${targetName} not accessible`;
+  }
+}
