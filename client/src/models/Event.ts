f5927863e797bdf7e357cfe4f4a6e89d0dae5e34
anqqa <anttiq@gmail.com>
2019-07-30 13:02:52 +0300

    Fix single image view

--- client/src/models/Event.ts
+++ client/src/models/Event.ts
@@ -3,7 +3,9 @@ import { BaseModel } from './BaseModel';
 
 export default class Event extends BaseModel {
   begins_at?: string;
+  city_name?: string;
   name?: string;
+  venue_name?: string;
 
   // resource() {
   //   return 'events';
--- client/src/models/Image.ts
+++ client/src/models/Image.ts
@@ -4,8 +4,17 @@ import { BaseModel } from '@/models/BaseModel';
 export default class Image extends BaseModel {
   color?: string;
   comment_count?: number;
+  comments?: any[];
+  created_at?: string;
+  description?: string;
+  exif?: any;
+  height?: number;
   id?: number;
+  notes?: any[];
+  tags?: any[];
   url?: string;
+  view_count?: number;
+  width?: number;
 
   // resource() {
   //   return 'images';
