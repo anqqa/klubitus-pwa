c25e97f7cb066c6d3d94768492a1f9e99721c3da
anqqa <anttiq@gmail.com>
2019-08-01 20:59:36 +0300

    Split the big image upload service into smaller and easier to manage parts, start with FileInterceptor simillar to express'

---
+++ api/src/common/interceptors/file.interceptor.ts
@@ -0,0 +1,135 @@
+import {
+  BadRequestException,
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  Logger,
+  NestInterceptor,
+} from '@nestjs/common';
+import { FastifyRequest } from 'fastify';
+import * as fs from 'fs-extra';
+import { extension } from 'mime-types';
+import { normalize } from 'path';
+import * as pump from 'pump';
+import { Observable, throwError } from 'rxjs';
+import { catchError, tap } from 'rxjs/operators';
+import { v4 as uuid } from 'uuid';
+
+const LOG_CONTEXT = 'FileInterceptor';
+
+interface File {
+  encoding?: string;
+  fieldname?: string;
+  filename?: string;
+  filesize?: number;
+  mimetype?: string;
+  originalname?: string;
+  tempfile?: string;
+  uuid?: string;
+}
+
+interface Request extends FastifyRequest {
+  file?: File;
+}
+
+interface Options {
+  uploadDir: string;
+}
+
+const defaultOptions: Options = {
+  uploadDir: './upload',
+};
+
+@Injectable()
+export class FileInterceptor implements NestInterceptor {
+  constructor(private fieldName: string, private options?: Partial<Options>) {}
+
+  async intercept(context: ExecutionContext, next: CallHandler): Promise<Observable<any>> {
+    const options = Object.assign({}, defaultOptions, this.options);
+
+    const ctx = context.switchToHttp();
+    const req = ctx.getRequest<Request>();
+    const body: any = {};
+    const file: File = {};
+    let shouldHandle = true;
+
+    Logger.debug(`Expecting field: ${this.fieldName}`, LOG_CONTEXT, false);
+
+    const uploadPath = normalize(options.uploadDir);
+    fs.ensureDirSync(uploadPath);
+
+    const streamConsumer = (
+      field: string,
+      stream: any,
+      filename: string,
+      encoding: string,
+      mimetype: string
+    ): void => {
+      shouldHandle = field === this.fieldName;
+
+      if (shouldHandle) {
+        Logger.debug(`Handling: ${filename} (${mimetype})`, LOG_CONTEXT);
+
+        file.uuid = uuid();
+        file.encoding = encoding;
+        file.fieldname = field;
+        file.filename = `${file.uuid}.${extension(mimetype)}`;
+        file.mimetype = mimetype;
+        file.originalname = filename;
+        file.tempfile = `${uploadPath}/${file.filename}`;
+
+        const writeStream = fs.createWriteStream(file.tempfile);
+
+        pump(stream, writeStream);
+      } else {
+        Logger.debug(`Skipping field: ${field}`, LOG_CONTEXT);
+
+        stream.resume();
+      }
+    };
+
+    const handleField = (key: string, value: any) => {
+      body[key] = value;
+    };
+
+    await new Promise((resolve, reject) => {
+      const multipart = req.multipart(streamConsumer, error => {
+        if (error) {
+          Logger.warn(`Failed: ${error.message}`, LOG_CONTEXT);
+
+          return reject(new BadRequestException(error));
+        }
+
+        req.body = body;
+
+        if (shouldHandle) {
+          file.filesize = fs.statSync(file.tempfile).size;
+          req.file = file;
+        }
+
+        resolve();
+      });
+
+      multipart.on('field', handleField);
+    });
+
+    const cleanUp = () => {
+      if (file.tempfile && fs.pathExistsSync(file.tempfile)) {
+        Logger.debug(`Removing ${file.tempfile}`, LOG_CONTEXT);
+
+        fs.removeSync(file.tempfile);
+      }
+    };
+
+    return next.handle().pipe(
+      tap(() => {
+        cleanUp();
+      }),
+      catchError(error => {
+        cleanUp();
+
+        return throwError(error);
+      })
+    );
+  }
+}
--- api/src/galleries/images/images.controller.ts
+++ api/src/galleries/images/images.controller.ts
@@ -1,12 +1,24 @@
-import { Controller, Post, Req, Res, UseGuards } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  NotFoundException,
+  Param,
+  Post,
+  Req,
+  UploadedFile,
+  UseGuards,
+  UseInterceptors,
+} from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
 import { ApiBearerAuth, ApiConsumes, ApiImplicitFile, ApiUseTags } from '@nestjs/swagger';
 import { Crud, CrudController } from '@nestjsx/crud';
-import { TransformerInterceptor } from '../../common/interceptors/transformer.interceptor';
+import * as fs from 'fs-extra';
 
+import { FileInterceptor } from '../../common/interceptors/file.interceptor';
+import { TransformerInterceptor } from '../../common/interceptors/transformer.interceptor';
+import { GalleriesService } from '../galleries.service';
 import { GalleryImage } from './image.entity';
 import { GalleryImagesService } from './images.service';
-import { ImageUploadService } from './imageupload.service';
 
 @Crud({
   model: { type: GalleryImage },
@@ -36,23 +48,36 @@ import { ImageUploadService } from './imageupload.service';
 @Controller('/galleries/:galleryId/images')
 export class GalleryImagesController implements CrudController<GalleryImage> {
   constructor(
-    readonly service: GalleryImagesService // readonly imageUploadService: ImageUploadService
+    readonly service: GalleryImagesService,
+    readonly galleriesService: GalleriesService
   ) {}
 
   get base(): CrudController<GalleryImage> {
     return this;
   }
 
-  // @ApiConsumes('multipart/form-data')
-  // @ApiImplicitFile({ name: 'file', required: true })
-  // @ApiBearerAuth()
-  // @UseGuards(AuthGuard())
-  // @Post('upload')
-  // async upload(@Req() request: any, @Res() response: any) {
-  //   try {
-  //     await this.imageUploadService.handleUpload(request, response);
-  //   } catch (error) {
-  //     return response.status(500).json(error.message);
-  //   }
-  // }
+  @ApiConsumes('multipart/form-data')
+  @ApiImplicitFile({ name: 'file', required: true })
+  @ApiBearerAuth()
+  @UseGuards(AuthGuard())
+  @UseInterceptors(new FileInterceptor('file'))
+  @Post()
+  async upload(@Param() params: any, @Body() body: any, @UploadedFile('file') file: any) {
+    const galleryId = parseInt(params.galleryId, 10);
+    const gallery = await this.galleriesService.findOne(galleryId);
+
+    if (!gallery) {
+      throw new NotFoundException(`Gallery ${galleryId} not found`);
+    }
+
+    // console.log({ gallery, galleryId, body, file });
+
+    return 'ok';
+    // return Promise.resolve('ok');
+    // try {
+    //   await this.imageUploadService.handleUpload(request, response);
+    // } catch (error) {
+    //   return response.status(500).json(error.message);
+    // }
+  }
 }
