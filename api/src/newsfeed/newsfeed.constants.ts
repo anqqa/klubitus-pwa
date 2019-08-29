46365e42b93c3cf6d977d551329d2c667742738f
anqqa <anttiq@gmail.com>
2019-08-29 15:08:09 +0300

    Upgrade API date-fns to v2

--- api/src/newsfeed/newsfeed.constants.ts
+++ api/src/newsfeed/newsfeed.constants.ts
@@ -1,8 +1,8 @@
 // tslint:disable:object-literal-sort-keys
 export const DATE_FORMATS: Record<string, string> = {
-  minute: 'YYYY-MM-DD-HH-mm',
-  hour: 'YYYY-MM-DD-HH',
-  day: 'YYYY-MM-DD',
-  week: 'YYYY-[W]WW',
-  month: 'YYYY-MM',
+  minute: 'yyyy-MM-dd-HH-mm',
+  hour: 'yyyy-MM-dd-HH',
+  day: 'yyyy-MM-dd',
+  week: "yyyy-'W'II",
+  month: 'yyyy-MM',
 };
