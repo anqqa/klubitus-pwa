01340ad4461826f9ccf5e2eae36ddec77fc4b6c9
anqqa <anttiq@gmail.com>
2019-10-07 14:17:10 +0300

    Throw conflict error on duplicate db entry

--- api/src/common/interceptors/errors.interceptor.ts
+++ api/src/common/interceptors/errors.interceptor.ts
@@ -1,5 +1,6 @@
 import {
   CallHandler,
+  ConflictException,
   ExecutionContext,
   ForbiddenException,
   Injectable,
@@ -8,9 +9,12 @@ import {
 } from '@nestjs/common';
 import { Observable, throwError } from 'rxjs';
 import { catchError } from 'rxjs/operators';
+import { QueryFailedError } from 'typeorm';
 import { EntityNotFoundError } from 'typeorm/error/EntityNotFoundError';
 
 import { EntityForbiddenError } from '../errors/entityforbidden.error';
+
+export const PG_UNIQUE_CONSTRAINT_VIOLATION = '23505';
 
 @Injectable()
 export class ErrorsInterceptor implements NestInterceptor {
@@ -26,6 +30,10 @@ const parseError = (error: Error): Error => {
 
   if (error instanceof EntityForbiddenError) {
     return new ForbiddenException(error.message);
+  }
+
+  if (error instanceof QueryFailedError && (error as any).code === PG_UNIQUE_CONSTRAINT_VIOLATION) {
+    return new ConflictException('Already exists');
   }
 
   return error;
