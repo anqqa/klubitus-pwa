156b83d6a0d283ea5551aadc2180b09c7971750e
Antti Qvickström <anttiq@gmail.com>
2019-11-04 11:39:34 +0200

    Upgrade @nestjsx/crud to alpha version to get auth support

--- api/src/common/basecrud.service.ts
+++ api/src/common/basecrud.service.ts
@@ -10,43 +10,37 @@ export class BaseCrudService<T> extends TypeOrmCrudService<T> {
     return false;
   }
 
-  protected getJoinType = (relationType: string) => 'leftJoin';
-
-  protected setJoin = (
-    cond: QueryJoin,
-    joinOptions: JoinOptions,
-    builder: SelectQueryBuilder<T>
-  ) => {
+  protected setJoin(cond: QueryJoin, joinOptions: JoinOptions, builder: SelectQueryBuilder<T>) {
     if (this.entityRelationsHash[cond.field] === undefined && cond.field.includes('.')) {
       const curr = this.getRelationMetadata(cond.field);
-
       if (!curr) {
         this.entityRelationsHash[cond.field] = null;
-
         return true;
       }
 
       this.entityRelationsHash[cond.field] = {
         name: curr.nestedRelation, // Fixes duplicate table name with multiple same name relations
         // name: curr.propertyName,
-        type: this.getJoinType(curr.relationType),
         columns: curr.inverseEntityMetadata.columns.map((col: any) => col.propertyName),
-        referencedColumn: (curr.joinColumns.length
-          ? curr.joinColumns[0]
-          : curr.inverseRelation.joinColumns[0]
-        ).referencedColumn.propertyName,
+        primaryColumns: curr.inverseEntityMetadata.primaryColumns.map(
+          (col: any) => col.propertyName
+        ),
         nestedRelation: curr.nestedRelation,
       };
     }
 
+    /* istanbul ignore else */
     if (cond.field && this.entityRelationsHash[cond.field] && joinOptions[cond.field]) {
       const relation = this.entityRelationsHash[cond.field];
       const options = joinOptions[cond.field];
       const allowed = this.getAllowedColumns(relation.columns, options);
 
+      /* istanbul ignore if */
       if (!allowed.length) {
         return true;
       }
+
+      const alias = options.alias ? options.alias : relation.name;
 
       const columns =
         !cond.select || !cond.select.length
@@ -54,20 +48,20 @@ export class BaseCrudService<T> extends TypeOrmCrudService<T> {
           : cond.select.filter(col => allowed.some((a: any) => a === col));
 
       const select = [
-        relation.referencedColumn,
+        ...relation.primaryColumns,
         ...(options.persist && options.persist.length ? options.persist : []),
         ...columns,
-      ].map(col => `${relation.name}.${col}`);
+      ].map(col => `${alias}.${col}`);
 
       const relationPath = relation.nestedRelation || `${this.alias}.${relation.name}`;
+      const relationType = options.required ? 'innerJoin' : 'leftJoin';
 
-      // @ts-ignore
-      builder[relation.type](relationPath, relation.name);
+      builder[relationType](relationPath, alias);
       builder.addSelect(select);
     }
 
     return true;
-  };
+  }
 
   protected mapOperatorsToQuery = (
     cond: QueryFilter,
