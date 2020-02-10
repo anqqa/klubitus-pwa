dae0ae59579faf70f13482685823e164c21c5973
Antti Qvickström <anttiq@gmail.com>
2020-02-10 17:40:29 +0200

    Use store to fetch forum posts

--- client/src/pages/forum/topic/_id/index.vue
+++ client/src/pages/forum/topic/_id/index.vue
@@ -6,26 +6,34 @@
 import { Component, Vue } from 'nuxt-property-decorator';
 
 import ForumPostList from '@/components/forum/ForumPostList.vue';
-import ForumTopic from '@/models/ForumTopic';
+import ForumPost from '@/models/ForumPost';
+import { Actions, forumNamespace, Getters, NAMESPACE } from '@/store/forum';
 
 @Component({
   components: { ForumPostList },
   watchQuery: ['page'],
 })
 export default class TopicPosts extends Vue {
-  async asyncData({ params, query }) {
-    const topic_id = parseInt(params.id);
+  @forumNamespace.Getter(Getters.POSTS_FOR_PAGE)
+  postsForPage!: (payload: { page: number; topicId: number }) => ForumPost[];
+
+  async fetch({ params, query, store }) {
+    const topicId = parseInt(params.id);
     const page = parseInt(query.page) || 1;
-    const limit = 20;
 
-    const posts = await new ForumTopic({ id: topic_id })
-      .posts()
-      .relation('author')
-      .limit(limit)
-      .page(page)
-      .get();
+    await store.dispatch(`${NAMESPACE}${Actions.LOAD_POSTS}`, { page, topicId });
+  }
 
-    return { posts };
+  get page(): number {
+    return parseInt(this.$route.query.page as string) || 1;
+  }
+
+  get posts(): ForumPost[] {
+    return this.postsForPage({ page: this.page, topicId: this.topicId });
+  }
+
+  get topicId(): number {
+    return parseInt(this.$route.params.id);
   }
 }
 </script>
--- client/src/store/forum.ts
+++ client/src/store/forum.ts
@@ -3,24 +3,29 @@ import { namespace } from 'nuxt-property-decorator';
 import { ActionTree, GetterTree, MutationTree } from 'vuex';
 
 import ForumArea from '@/models/ForumArea';
+import ForumPost from '@/models/ForumPost';
 import ForumTopic from '@/models/ForumTopic';
 
 interface ForumState {
   activeTopicId?: number;
   areas: ForumArea[];
-  latestTopicIds: number[];
+  latestTopicIds?: number[];
+  postPages: Record<number, Record<number, number[]>>;
+  posts: Record<number, ForumPost>;
   topics: Record<number, ForumTopic>;
 }
 
 export const state = (): ForumState => ({
   areas: [],
-  latestTopicIds: [],
+  postPages: {},
+  posts: {},
   topics: {},
 });
 
 export const enum Actions {
   LOAD_AREAS = 'loadAreas',
   LOAD_LATEST_TOPICS = 'loadLatestTopics',
+  LOAD_POSTS = 'loadPosts',
   LOAD_TOPIC = 'loadTopic',
 }
 
@@ -56,6 +61,29 @@ export const actions: ActionTree<ForumState, any> = {
     );
   },
 
+  async [Actions.LOAD_POSTS]({ commit, state }, payload: { page: number; topicId: number }) {
+    const { page, topicId } = payload;
+
+    if (topicId in state.postPages && page in state.postPages[topicId]) {
+      return;
+    }
+
+    const limit = 20;
+    const posts = await new ForumTopic({ id: topicId })
+      .posts()
+      .relation('author')
+      .limit(limit)
+      .page(page)
+      .get();
+    const postIds = posts.map(post => post.id!);
+
+    commit(
+      Mutations.SET_POSTS,
+      posts.map(post => post.data())
+    );
+    commit(Mutations.SET_POST_PAGES, { page, postIds, topicId });
+  },
+
   async [Actions.LOAD_TOPIC](
     { commit, state },
     payload: { activate?: boolean; force?: boolean; topicId: number }
@@ -81,6 +109,7 @@ export const enum Getters {
   ACTIVE_TOPIC = 'activeTopic',
   GROUPED_AREAS = 'groupedAreas',
   LATEST_TOPICS = 'latestTopics',
+  POSTS_FOR_PAGE = 'postsForPage',
 }
 
 export const getters: GetterTree<ForumState, any> = {
@@ -104,20 +133,49 @@ export const getters: GetterTree<ForumState, any> = {
     return groups;
   },
 
-  [Getters.LATEST_TOPICS]: state => state.latestTopicIds.map(id => state.topics[id]),
+  [Getters.LATEST_TOPICS]: state => state.latestTopicIds?.map(id => state.topics[id]),
+
+  [Getters.POSTS_FOR_PAGE]: state => (payload: { page: number; topicId: number }) => {
+    const { page, topicId } = payload;
+    const posts: ForumPost[] = [];
+    const postIds = (state.postPages[topicId] || {})[page] || [];
+
+    postIds.forEach(id => id in state.posts && posts.push(state.posts[id]));
+
+    return posts;
+  },
 };
 
 export const enum Mutations {
   SET_ACTIVE_TOPIC = 'setActiveTopic',
   SET_AREAS = 'setAreas',
   SET_LATEST_TOPICS = 'setLatestTopics',
+  SET_POST_PAGES = 'setPostPages',
+  SET_POSTS = 'setPosts',
   SET_TOPICS = 'setTopics',
 }
 
 export const mutations: MutationTree<ForumState> = {
-  [Mutations.SET_ACTIVE_TOPIC]: (state, id?: number) => (state.activeTopicId = id),
+  [Mutations.SET_ACTIVE_TOPIC]: (state, id?: number) => {
+    state.activeTopicId = id;
+  },
+
   [Mutations.SET_AREAS]: (state, areas: ForumArea[]) => (state.areas = areas),
+
   [Mutations.SET_LATEST_TOPICS]: (state, ids: number[]) => (state.latestTopicIds = ids),
+
+  [Mutations.SET_POST_PAGES]: (
+    state,
+    payload: { page: number; postIds: number[]; topicId: number }
+  ) => {
+    const { page, postIds, topicId } = payload;
+
+    state.postPages[topicId] = { ...(state.postPages[topicId] || {}), [page]: postIds };
+  },
+
+  [Mutations.SET_POSTS]: (state, posts: ForumPost[]) =>
+    posts.forEach(post => (state.posts[post.id!] = post)),
+
   [Mutations.SET_TOPICS]: (state, topics: ForumTopic[]) =>
     topics.forEach(topic => (state.topics[topic.id!] = topic)),
 };
